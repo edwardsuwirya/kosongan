c106484e7556275a56644714dd69ac9334e3a394
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-01-14 09:38:35 +0700

    Add book category

--- src/main/java/com/enigmacamp/kosongan/controller/KosonganController.java
+++ src/main/java/com/enigmacamp/kosongan/controller/KosonganController.java
@@ -1,6 +1,7 @@
 package com.enigmacamp.kosongan.controller;
 
 import com.enigmacamp.kosongan.model.Book;
+import com.enigmacamp.kosongan.model.BookCategory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,5 +12,10 @@ public class KosonganController {
     @GetMapping("/books")
     public ResponseEntity<Book> doGetAllBook() {
         return new ResponseEntity(new Book("123", "Novel"), HttpStatus.OK);
+    }
+
+    @GetMapping("/bookCategories")
+    public ResponseEntity<BookCategory> doGetAllBookCategory() {
+        return new ResponseEntity(new BookCategory("123", "Anak2"), HttpStatus.OK);
     }
 }
